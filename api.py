3ed8350fb9744cfb14bcb9fc7d9f49c5f9324d61
RaspTomo <raspberry.tomo@gmail.com>
2017-07-09 14:33:51 +0900

    Add PUT method

--- api.py
+++ api.py
@@ -15,6 +15,7 @@ class Quote(peewee.Model):
 
 api = Flask(__name__)
 
+#Select
 @api.route('/1.0/getQuote/<string:code>', methods=['GET'])
 def get_quote(code):
     try:
@@ -35,11 +36,24 @@ def get_quote(code):
     # Unicodeにしたくない場合は↓
     #return make_response(json.dumps(result, ensure_ascii=False))
 
+#Update
+@api.route('/1.0/getQuote/<string:code>', methods=['PUT'])
+def put_quote(code):
+    dataDict = json.loads(request.data)
+    try:
+        q = Quote.update(code=dataDict["code"], price=dataDict["price"], name=dataDict["name"]).where(Quote.code == dataDict["code"])
+    except Quote.DoesNotExist:
+        abort(404)
+
+    result = {
+        "result":True
+        }
+
+    return make_response(jsonify(result))
+
 @api.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
     api.run(host='0.0.0.0', port=3000)
-
-
